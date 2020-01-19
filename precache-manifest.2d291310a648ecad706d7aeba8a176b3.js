5f6047ac885e74ae01856d8788016f22d100e310
rhlmalik17 <rhlmalik17@gmail.com>
2020-01-20 00:54:50 +0530

    Updates

---
+++ precache-manifest.2d291310a648ecad706d7aeba8a176b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6884b37714dc9deb40eb9806b8e8fd",
+    "url": "/deploy-app/index.html"
+  },
+  {
+    "revision": "24b8daa8ac45ac075530",
+    "url": "/deploy-app/static/css/main.6e06c00b.chunk.css"
+  },
+  {
+    "revision": "c6eef0c3d4abf6ded5f8",
+    "url": "/deploy-app/static/js/2.4a3f703e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-app/static/js/2.4a3f703e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24b8daa8ac45ac075530",
+    "url": "/deploy-app/static/js/main.5e47cd7e.chunk.js"
+  },
+  {
+    "revision": "a67821f4fa16998efab3",
+    "url": "/deploy-app/static/js/runtime-main.964815d9.js"
+  },
+  {
+    "revision": "1295343e3e8e4f07f766b72ef593490c",
+    "url": "/deploy-app/static/media/DarthVader.1295343e.png"
+  },
+  {
+    "revision": "47ad59044794fe4531e74445d2628039",
+    "url": "/deploy-app/static/media/Yoda.47ad5904.png"
+  }
+]);
